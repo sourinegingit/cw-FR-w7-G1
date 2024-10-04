5295020cedcaab9b09f51b1197a6ac29f37f18ba
masoud_jalili <masoudjalili0781@gmail.com>
2024-10-04 16:19:35 +0330

    q1 finisshed finally

--- q1.js
+++ q1.js
@@ -1,44 +1,15 @@
 const icon = document.querySelector(".icon");
 const form = document.querySelector(".form");
-// const input=document.querySelectorAll("input")
-// console.log(input);
-
-// const label=document.querySelectorAll("label")
-// const button=document.querySelector("button")
-// const container=document.querySelector(".container")
-// // console.log(icon);
-// icon.addEventListener('click',changeColor)
-
-// function changeColor() {
-
-//     container.style.backgroundColor="gray";
-
-//     button.style.backgroundColor="green";
-//     form.style.backgroundColor="white";
-
-// }
-// console.log(input);
-// Array.from(input).map((item)=>{
-//     // console.log(item);
-
-//     })
-
 icon.addEventListener("click", () => {
   changeFormTheme(form);
 });
 
 function changeFormTheme(form) {
   let theme = form.getAttribute("data-theme");
-//   theme = "dark"
-//   if (theme === "dark") {
-//     theme = "light";
 
-//   } else {
-//     theme = "dark";
-
-//   }
-//   console.log(theme);
-
-
-theme = (theme === 'dark') ? 'light' : 'dark';
+  if (theme === "dark") {
+    form.setAttribute("data-theme", "light");
+  } else {
+    form.setAttribute("data-theme", "dark");
+  }
 }
