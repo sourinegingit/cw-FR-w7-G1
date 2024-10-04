0886dbeceff8c4a4e578a94f50b7c612780b9ab7
𝓕𝓽𝓶 <s.sh.d5232@gmail.com>
2024-10-04 15:55:38 +0330

    not final

--- q1.js
+++ q1.js
@@ -1,27 +1,44 @@
-const icon=document.querySelector(".icon");
-const form=document.querySelector(".form")
-const input=document.querySelectorAll("input")
-console.log(input);
+const icon = document.querySelector(".icon");
+const form = document.querySelector(".form");
+// const input=document.querySelectorAll("input")
+// console.log(input);
 
-const label=document.querySelectorAll("label")
-const button=document.querySelector("button")
-const container=document.querySelector(".container")
-// console.log(icon);
-icon.addEventListener('click',changeColor)
+// const label=document.querySelectorAll("label")
+// const button=document.querySelector("button")
+// const container=document.querySelector(".container")
+// // console.log(icon);
+// icon.addEventListener('click',changeColor)
+
+// function changeColor() {
+
+//     container.style.backgroundColor="gray";
+
+//     button.style.backgroundColor="green";
+//     form.style.backgroundColor="white";
+
+// }
+// console.log(input);
+// Array.from(input).map((item)=>{
+//     // console.log(item);
+
+//     })
+
+icon.addEventListener("click", () => {
+  changeFormTheme(form);
+});
+
+function changeFormTheme(form) {
+  let theme = form.getAttribute("data-theme");
+//   theme = "dark"
+//   if (theme === "dark") {
+//     theme = "light";
+
+//   } else {
+//     theme = "dark";
+
+//   }
+//   console.log(theme);
 
 
-
-
-function changeColor() {
-    console.log(input);
-    
-    container.style.backgroundColor="gray";
-    input.map((item)=>{
-        item.addEventListener("input",()=>{console.log(item);
-        })
-    })
-    
-    button.style.backgroundColor="green";
-    form.style.backgroundColor="white";
-    
+theme = (theme === 'dark') ? 'light' : 'dark';
 }
