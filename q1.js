229b979c83a260d3cd1038190611c567595418a7
masoud_jalili <masoudjalili0781@gmail.com>
2024-10-06 14:02:52 +0330

    question 2

--- q1.js
+++ q1.js
@@ -13,70 +13,45 @@ function changeFormTheme(form) {
     form.setAttribute("data-theme", "dark");
   }
 }
+
 // question 2----------------------------------
 
-// const data = [
-//   { name: "banana", count: 2, pricePerOne: 45000, discount: 5 },
-//   { name: "potato", count: 8, pricePerOne: 23000, discount: 15 },
-//   { name: "onion", count: 12, pricePerOne: 12000, discount: 10 },
-// ];
-
-function factor(data) {
-  let sum = 0;
-
-  return data.map((item) => {
-    const discountAmount = (item.pricePerOne * item.discount) / 100;
-    const finalPrice = item.pricePerOne - discountAmount;
-    sum += finalPrice * item.count;
-    return {
-      name: item.name,
-      count: item.count,
-      pricePerOne: item.pricePerOne,
-      discount: item.discount,
-      finalPrice: finalPrice,
-      totalprice: sum,
-    };
-  });
-}
-let info = factor(data);
-
-//DOM
-
-let table = document.createElement("table");
-table.className = "myTable";
-
-// Create a header row
-let thead = table.createTHead();
-let row = thead.insertRow();
-let headers = [
-  "row",
-  "Product name",
-  "count",
-  "price per one before discount",
-  "price per one after discount",
-  "Total",
-];
-headers.forEach((headerText) => {
-  let th = document.createElement("th");
-  th.appendChild(document.createTextNode(headerText));
-  row.appendChild(th);
-});
-
-// Create a body for your table
-let tbody = table.createTBody();
-let data = [
+const data = [
   { name: "banana", count: 2, pricePerOne: 45000, discount: 5 },
   { name: "potato", count: 8, pricePerOne: 23000, discount: 15 },
   { name: "onion", count: 12, pricePerOne: 12000, discount: 10 },
 ];
 
-info.forEach((item) => {
-  let row = tbody.insertRow();
-  Object.values(item).forEach((text) => {
-    let cell = row.insertCell();
-    cell.appendChild(document.createTextNode(text));
+//function for calcaluate discount refer to question 4 week 4
+function dataHandle(data) {
+  let sum = 0;
+  return data.map((item) => {
+    const discountAmount = (item.pricePerOne * item.discount) / 100;
+    const finalPrice = item.pricePerOne - discountAmount;
+    sum += finalPrice * item.count;
+    return {
+      1: item.name,
+      2: item.count,
+      3: item.pricePerOne,
+      4: finalPrice,
+      5: sum,
+    };
   });
-});
+}
 
-// Append the table to the body of the document
-document.body.appendChild(table);
+//dom for assign data to table
+
+const finalData = dataHandle(data);
+for (let i = 1; i <= 5; i++) {
+  let td = document.querySelector(`.b${i}`);
+  td.innerHTML = `${finalData[0][i]}`;
+}
+
+for (let i = 1; i <= 5; i++) {
+  let td = document.querySelector(`.p${i}`);
+  td.innerHTML = `${finalData[1][i]}`;
+}
+for (let i = 1; i <= 5; i++) {
+  let td = document.querySelector(`.o${i}`);
+  td.innerHTML = `${finalData[2][i]}`;
+}
